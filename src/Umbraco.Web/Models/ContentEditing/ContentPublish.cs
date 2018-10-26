8b7546a20b4526e5227311f3814641f2375132e6
Warren Buckley <warren@umbraco.com>
2018-10-26 11:13:54 +0100

    New model for the updated WebAPI POST method to take rather than just an int id of the node

---
+++ src/Umbraco.Web/Models/ContentEditing/ContentPublish.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Umbraco.Web.Models.ContentEditing
+{
+    [DataContract(Name = "contentPublish", Namespace = "")]
+    public class ContentPublish
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "includeDescendants")]
+        public bool IncludeDescendants { get; set; }
+
+        [DataMember(Name = "publishDrafts")]
+        public bool PublishDrafts { get; set; }
+
+        [DataMember(Name = "cultures")]
+        public string[] Cultures { get; set; }
+    }
+}
