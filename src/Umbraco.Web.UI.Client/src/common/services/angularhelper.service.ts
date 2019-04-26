75d6e02c631e75cec7b184cca8e1409fbe458e43
Peter Duncanson <pete@offroadcode.com>
2019-04-26 01:25:59 +0100

    Changed iHasScope to "any" type instead of "object" as we don't know what shape it might be

--- src/Umbraco.Web.UI.Client/src/common/services/angularhelper.service.ts
+++ src/Umbraco.Web.UI.Client/src/common/services/angularhelper.service.ts
@@ -1,19 +1,16 @@
 /// <reference path="../definitions/global.d.ts" />
 
 module umbraco.services {
-
     export class angularHelper {
-
         public revalidateNgModel(scope, ngModel) {
-            this.safeApply(scope, function () {
-                angular.forEach(ngModel.$parsers, function (parser) {
+            this.safeApply(scope, function() {
+                angular.forEach(ngModel.$parsers, function(parser) {
                     parser(ngModel.$viewValue);
                 });
             });
         }
 
         public executeSequentialPromises(promises) {
-
             //this is sequential promise chaining, it's not pretty but we need to do it this way.
             //$q.all doesn't execute promises in sequence but that's what we want to do here.
 
@@ -25,19 +22,17 @@ module umbraco.services {
             var j = 0;
             function pExec(promise) {
                 j++;
-                return promise.then(function (data) {
+                return promise.then(function(data) {
                     if (j === promises.length) {
                         return $q.when(data); //exit
-                    }
-                    else {
+                    } else {
                         return pExec(promises[j]); //recurse
                     }
                 });
             }
             if (promises.length > 0) {
                 return pExec(promises[0]); //start the promise chain
-            }
-            else {
+            } else {
                 return $q.when(true); // just exit, no promises to execute
             }
         }
@@ -47,43 +42,50 @@ module umbraco.services {
                 if (angular.isFunction(fn)) {
                     fn();
                 }
-            }
-            else {
+            } else {
                 if (angular.isFunction(fn)) {
                     scope.$apply(fn);
-                }
-                else {
+                } else {
                     scope.$apply();
                 }
             }
         }
 
         public getCurrentForm(scope) {
-
             //NOTE: There isn't a way in angular to get a reference to the current form object since the form object
             // is just defined as a property of the scope when it is named but you'll always need to know the name which
             // isn't very convenient. If we want to watch for validation changes we need to get a form reference.
-            // The way that we detect the form object is a bit hackerific in that we detect all of the required properties 
+            // The way that we detect the form object is a bit hackerific in that we detect all of the required properties
             // that exist on a form object.
             //
             //The other way to do it in a directive is to require "^form", but in a controller the only other way to do it
             // is to inject the $element object and use: $element.inheritedData('$formController');
 
             var form = null;
-            var requiredFormProps = ["$error", "$name", "$dirty", "$pristine", "$valid", "$submitted", "$pending"];
+            var requiredFormProps = [
+                "$error",
+                "$name",
+                "$dirty",
+                "$pristine",
+                "$valid",
+                "$submitted",
+                "$pending"
+            ];
 
             // a method to check that the collection of object prop names contains the property name expected
             function propertyExists(objectPropNames) {
                 //ensure that every required property name exists on the current scope property
-                return _.every(requiredFormProps, function (item) {
-
+                return _.every(requiredFormProps, function(item) {
                     return _.contains(objectPropNames, item);
                 });
             }
 
             for (var p in scope) {
-
-                if (_.isObject(scope[p]) && p !== "this" && p.substr(0, 1) !== "$") {
+                if (
+                    _.isObject(scope[p]) &&
+                    p !== "this" &&
+                    p.substr(0, 1) !== "$"
+                ) {
                     //get the keys of the property names for the current property
                     var props = _.keys(scope[p]);
                     //if the length isn't correct, try the next prop
@@ -132,9 +134,11 @@ module umbraco.services {
 
     export namespace models {
         export interface iHasScope {
-            scope: object
+            scope: any;
         }
     }
 }
 
-angular.module('umbraco.services').service('angularHelper', umbraco.services.angularHelper);
+angular
+    .module("umbraco.services")
+    .service("angularHelper", umbraco.services.angularHelper);
