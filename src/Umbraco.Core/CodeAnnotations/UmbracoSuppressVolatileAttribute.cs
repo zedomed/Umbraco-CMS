7c84fc0cfd3c1e1ecee03926cfff9cd9b35bfa1e
Nikolaj <nel@umbraco.dk>
2020-08-20 10:56:47 +0200

    Add attributes

---
+++ src/Umbraco.Core/CodeAnnotations/UmbracoSuppressVolatileAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Umbraco.Core.CodeAnnotations
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class UmbracoSuppressVolatileAttribute : Attribute
+    {
+    }
+}
