c126330f8b1efb701b2672a14406e4c0826ff111
Nikolaj <nel@umbraco.dk>
2020-08-20 11:20:28 +0200

    Add comments to volatile attributes

--- src/Umbraco.Core/CodeAnnotations/UmbracoSuppressVolatileAttribute.cs
+++ src/Umbraco.Core/CodeAnnotations/UmbracoSuppressVolatileAttribute.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace Umbraco.Core.CodeAnnotations
 {
+    /// <summary>
+    /// Assembly level attribute which supresses 
+    /// an UmbracoVolatile error to a warning
+    /// </summary>
     [AttributeUsage(AttributeTargets.Assembly)]
     public class UmbracoSuppressVolatileAttribute : Attribute
     {
--- src/Umbraco.Core/CodeAnnotations/UmbracoVolatileAttribute.cs
+++ src/Umbraco.Core/CodeAnnotations/UmbracoVolatileAttribute.cs
@@ -5,6 +5,12 @@ using System.Text;
 
 namespace Umbraco.Core.CodeAnnotations
 {
+    /// <summary>
+    /// Attribute which marks a method or class as volatile, 
+    /// causing the method, or any method from the marked class,
+    /// to issue an error if invoked.
+    /// Can be suppressed to a warning with UmbracoSuppressVolatileAttribute.
+    /// </summary>
     public class UmbracoVolatileAttribute : Attribute
     {
 
